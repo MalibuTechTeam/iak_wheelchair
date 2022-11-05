a9bd92f43bad487341e7037ed4c63b73ee81b4c6
Moldrok <alessio.aquilano@outlook.it>
2022-11-05 19:26:00 +0100

    v1.0.1

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author "Iakko(Malibù Tech Team)"
+
+description 'A drivable wheelchair'
+
+version '1.0.1'
+
+files {
+	'data/vehicles.meta',
+	'data/carvariations.meta',
+	'data/handling.meta'
+}
+
+data_file 'HANDLING_FILE'			'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'	'data/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE'	'data/carvariations.meta'
